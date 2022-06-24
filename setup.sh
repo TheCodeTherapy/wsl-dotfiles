88c93b164f20308a9ecf8f3af13d905a5bf605cf
TheCodeTherapy <me@mgz.me>
2022-06-24 12:24:43 +0100

    updates setup script and windows terminal profile

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -eu -o pipefail
 ME="/home/$(whoami)"
 CFG="$ME/.config"
 DOTDIR="${ME}/wsl-dotfiles"
+NVMDIR="${ME}/.nvm"
 BINDIR="${DOTDIR}/bin"
 
 declare -rA COLORS=(
@@ -18,16 +19,26 @@ declare -rA COLORS=(
     [OFF]=$'\033[0m'
 )
 
-print_success () {
+print_red () {
+    echo -e "\n${COLORS[RED]}${1}${COLORS[OFF]}\n"
+}
+
+print_yellow () {
+    echo -e "\n${COLORS[YELLOW]}${1}${COLORS[OFF]}\n"
+}
+
+print_green () {
     echo -e "\n${COLORS[GREEN]}${1}${COLORS[OFF]}\n"
 }
 
-print_info () {
+print_cyan () {
     echo -e "\n${COLORS[CYAN]}${1}${COLORS[OFF]}\n"
 }
 
-print_fail () {
-    echo -e "\n${COLORS[RED]}${1}${COLORS[OFF]}\n"
+wait_key () {
+    echo -e "\n${COLORS[YELLOW]}"
+    read -n 1 -s -r -p "${1}"
+    echo -e "${COLORS[OFF]}\n"
 }
 
 home_link () {
@@ -35,7 +46,7 @@ home_link () {
         && ln -s $DOTDIR/$1 $ME/$2 \
         || ln -s $DOTDIR/$1 $ME/$2
     msg="# Linked $DOTDIR/$1 to -> $ME/$2"
-    print_info "${msg}"
+    print_cyan "${msg}"
 }
 
 home_link_cfg () {
@@ -44,12 +55,12 @@ home_link_cfg () {
         && ln -s $DOTDIR/$1 $CFG/. \
         || ln -s $DOTDIR/$1 $CFG/.
     msg="# Linked $DOTDIR/$1 to dir -> $CFG/$1"
-    print_info "${msg}"
+    print_cyan "${msg}"
 }
 
 choose_fastest_mirror () {
     msg="# Checking mirrors speed (please wait)..."
-    print_success "${msg}"
+    print_green "${msg}"
     fastest=$(curl -s http://mirrors.ubuntu.com/mirrors.txt \
         | xargs -n1 -I {} sh -c 'echo `curl -r 0-102400 -s -w %{speed_download} -o /dev/null {}/ls-lR.gz` {}' \
         | sort -g -r \
@@ -68,18 +79,19 @@ choose_fastest_mirror () {
     list="$list\n$mirror $cn-security main restricted"
     list="$list\n$mirror $cn-security universe"
     list="$list\n$mirror $cn-security multiverse"
-    echo -e $list | sudo tee /etc/apt/sources.list
+    # echo -e $list | sudo tee /etc/apt/sources.list
+    echo -e $list
 }
 
 update_system () {
     msg="# Updating your system (please wait)..."
-    print_success "${msg}"
+    print_green "${msg}"
     sudo apt -y update && sudo apt -y upgrade
 }
 
 install_basic_packages () {
     msg="# Installing basic packages (please wait)..."
-    print_success "${msg}"
+    print_green "${msg}"
     sudo apt -y install unzip lzma tree neofetch build-essential autoconf \
         automake cmake cmake-data pkg-config clang git neovim zsh python3 \
         ipython3 python3-pip python3-dev python-is-python3 tmux
@@ -87,63 +99,68 @@ install_basic_packages () {
 }
 
 install_nvm () {
-    msg="# Installing nvm (please wait)..."
-    print_success "${msg}"
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-    source $ME/.bashrc
+    msg="INSTALLING NVM ..."
+    print_yellow "${msg}"
+    if [[ -f $NVMDIR/nvm.sh ]]; then
+        print_green "nvm already installed."
+    else
+        curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+    fi
 }
 
 install_node () {
-    if [ -z $NVM_DIR ]; then
-        if [[ -f $ME/.nvm/nvm.sh ]]; then
-            export NVM_DIR="$HOME/.nvm"
-            [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-            [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+    msg="INSTALLING NODEJS ..."
+    print_yellow "${msg}"
+    if [[ -f $NVMDIR/nvm.sh ]]; then
+        if $(npm --version > /dev/null 2>&1); then
+            msg="npm already installed."
+            print_green "${msg}"
         else
-            install_nvm
+            source $NVMDIR/nvm.sh
+            VER=$(nvm ls-remote --lts | grep "Latest" | tail -n 1 | sed 's/[-/a-zA-Z]//g' | sed 's/^[ \t]*//')
+            msg="Installing Latest NodeJS version found: ${VER}"
+            print_yellow "${msg}"
+            nvm install $VER
         fi
-        msg="# Installing NodeJS (please wait)..."
-        print_success "${msg}"
-        nvm install 14.16.0
     else
-        if $(nvm --version > /dev/null 2>&1); then
-            msg="# Installing NodeJS (please wait)..."
-            print_success "${msg}"
-            nvm install 14.16.0
-        fi
+        msg="nvm not installed."
+        print_red "${msg}"
     fi
 }
 
 install_yarn_package () {
     if $(yarn --version > /dev/null 2>&1); then
         msg="Yarn already installed."
-        print_success "${msg}"
+        print_green "${msg}"
     else
         msg="Installing Yarn package..."
-        print_info "${msg}"
+        print_yellow "${msg}"
         sudo apt update && sudo apt -y install yarn
     fi
 }
 
 install_yarn () {
     msg="Installing Yarn..."
-    print_info "${msg}"
-    if $(APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key list | grep "Yarn Packaging" > /dev/null 2>&1); then
+    print_yellow "${msg}"
+    if [[ -f /etc/apt/trusted.gpg.d/yarn.gpg ]]; then
         msg="Yarn GPG key already added to system."
-        print_success "${msg}"
+        print_green "${msg}"
     else
         msg="Adding Yarn GPG key to system..."
-        print_info "${msg}"
-        curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+        print_yellow "${msg}"
+        curl https://dl.yarnpkg.com/debian/pubkey.gpg \
+            | gpg --dearmor \
+            | sudo tee /etc/apt/trusted.gpg.d/yarn.gpg > /dev/null
     fi
     if [[ -f /etc/apt/sources.list.d/yarn.list ]]; then
         msg="Yarn sources list already added to system."
-        print_success "${msg}"
+        print_green "${msg}"
         install_yarn_package
     else
         msg="Adding yarn.list to sources.list.d..."
-        print_info "${msg}"
-        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+        print_yellow "${msg}"
+        echo "deb https://dl.yarnpkg.com/debian/ stable main" \
+            | sudo tee /etc/apt/sources.list.d/yarn.list
         install_yarn_package
     fi
 }
@@ -151,17 +168,17 @@ install_yarn () {
 install_awscli () {
     if $(aws --version > /dev/null 2>&1); then
         msg="AWS CLI already installed."
-        print_success "${msg}"
+        print_green "${msg}"
     else
         msg="Installing AWS CLI..."
-        print_info "${msg}"
+        print_cyan "${msg}"
         pip3 install awscli --upgrade --user
     fi
 }
 
 install_nvim () {
     msg="# Installing latest neovim (please wait)..."
-    print_success "${msg}"
+    print_green "${msg}"
     sudo add-apt-repository -y ppa:neovim-ppa/stable
     sudo apt -y update
     sudo apt -y install neovim
@@ -171,22 +188,24 @@ install_nvim () {
 install_exa () {
     if [[ -f $BINDIR/exa ]]; then
         msg="Exa already installed."
-        print_success "${msg}"
+        print_green "${msg}"
     else
         msg="# Downloading Exa (please wait)..."
-        print_success "${msg}"
+        print_green "${msg}"
         cd $BINDIR \
-            && wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip \
-            && unzip exa-linux-x86_64-0.9.0.zip \
-            && rm exa-linux-x86_64-0.9.0.zip \
-            && mv exa-linux-x86_64 exa \
+            && mkdir exa-10.0.1 && cd exa-10.0.1 \
+            && wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip \
+            && unzip exa-linux-x86_64-v0.10.1.zip \
+            && rm exa-linux-x86_64-v0.10.1.zip \
+            && cd $BINDIR \
+            && ln -s ./exa-10.0.1/bin/exa . \
             && cd ..
     fi
 }
 
 cd $ME
 
-choose_fastest_mirror
+# choose_fastest_mirror
 update_system
 install_basic_packages
 install_nvim
@@ -208,7 +227,7 @@ fi
 
 if $(node --version > /dev/null 2>&1); then
     msg="NodeJS already installed."
-    print_success "${msg}"
+    print_green "${msg}"
 else
     install_node
 fi
